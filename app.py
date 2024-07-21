f284eb874d10d637def03897e5fe16ec2170a071
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2024-07-21 17:00:25 +0100

    chore: Add course router to the app

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from config.config import Settings
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_versioning import VersionedFastAPI
-from fastapi import FastAPI, HTTPException, Request
-from pydantic import ValidationError as PydanticValidationError
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI
+
 
 from database.mongod import init_db
+
+
+
+
+from routes.v1 import course
 
 
 app = FastAPI(
@@ -16,22 +19,16 @@ app = FastAPI(
 )
 
 
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request: Request, exc: HTTPException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"error": exc.detail},
-    )
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=422,
-        content={
-            "error": "Validation error",
-            "details": exc.errors()
-        }
-    )
+
+# add routers
+app.include_router(course.router)
+
+
+
+app = VersionedFastAPI(app, enable_latest=True, version_format='{major}',
+    prefix_format='/v{major}')
+
 
 
 
@@ -53,21 +50,13 @@ async def startup():
 
 app.add_event_handler("startup", startup)
 
-@app.get('/')
-def index():
-    return {
-        "API": {
-            "version": Settings().APP_VERSION,
-            "description": Settings().APP_DESCRIPTION
-        }
-    }
-
-
 @app.get("/healthcheck")
 def api_healthcheck():
     return "OK 200 - app running successfully"
 
 
-app = VersionedFastAPI(app, enable_latest=True, version_format='{major}',
-    prefix_format='/v{major}')
 
+
+
+# # register routers
+# app.include_router(course.router)
