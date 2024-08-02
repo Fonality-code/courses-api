dc66cebd105a7ac916afb210b0cd4d5c5b4aa5c5
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2024-08-02 23:08:18 +0100

    chore: Update app configuration and dependencies

--- config/config.py
+++ config/config.py
@@ -6,8 +6,8 @@ class Settings(BaseSettings):
         env_file=".env", env_file_encoding="utf-8", from_attributes=True, extra="ignore"
     )
 
-    APP_NAME: str
-    APP_VERSION: str
+    APP_NAME: str = "API"
+    APP_VERSION: str = "1.0.0"
     APP_DESCRIPTION: str = ""
     MONGODB_URL: str
     DATABASE_NAME: str
